347cf217759e52777b2447982c7c29263cfa0035
János Arenhövel <jaarenhoevel@gmail.com>
2023-10-07 16:38:42 +0200

    Create beacon framework

--- lib/Controller/Controller.cpp
+++ lib/Controller/Controller.cpp
@@ -32,6 +32,32 @@ void Controller::update() {
 void Controller::setEffect(Effect *effect, uint32_t param1, uint32_t param2, uint32_t param3, uint32_t param4) {
     effect->init(param1, param2, param3, param4);
     this->currentEffect = effect;
+
+    this->effectParams[0] = param1;
+    this->effectParams[1] = param2;
+    this->effectParams[2] = param3;
+    this->effectParams[3] = param4;
+
+    if (this->isMaster) this->broadcastStatus();
+}
+
+Effect* Controller::getEffectById(uint16_t id) {
+    switch (id) {
+    case Controller::Effects::BLINDER:
+        return this->blinderEffect;
+
+    case Controller::Effects::RAINBOW:
+        return this->rainbowEffect;
+
+    case Controller::Effects::ROTATING_BEAM:
+        return this->rotatingBeamEffect;
+
+    case Controller::Effects::STROBE:
+        return this->strobeEffect;
+    
+    default:
+        break;
+    }   
 }
 
 void Controller::nextEffect() {
@@ -55,6 +81,7 @@ void Controller::nextEffect() {
         break;
     
     default:
+        this->setEffect(this->getEffectById(this->effectIndex));
         break;
     }
 
@@ -66,4 +93,39 @@ void Controller::nextEffect() {
 
 uint32_t Controller::getMillis() {
     return millis() + this->millisOffset;
+}
+
+void Controller::broadcastStatus() {
+    beacon status;
+
+    status.magic = MAGIC;
+    status.millis = millis();
+    status.effect = this->effectIndex;
+    status.param1 = this->effectParams[0];
+    status.param2 = this->effectParams[1];
+    status.param3 = this->effectParams[2];
+    status.param4 = this->effectParams[3];
+}
+
+void Controller::handleBeacon(const uint8_t * mac, const uint8_t *data, int len) {
+    beacon* status =(beacon*) data;
+
+    if (status->magic != MAGIC) {
+        Serial.printf("Magic bytes did not match!");
+        return;
+    }
+
+    if (status->millis < millis()) {
+        this->isMaster = true;
+        this->millisOffset = 0;
+
+        Serial.printf("I am the master :)");
+        return;
+    }
+
+    this->isMaster = false;
+    this->millisOffset = status->millis - millis();
+    Serial.printf("Received master millis :( adjusting offset");
+
+    this->setEffect(this->getEffectById(status->effect), status->param1, status->param2, status->param3, status->param4);
 }
--- lib/Controller/Controller.h
+++ lib/Controller/Controller.h
@@ -11,6 +11,8 @@
 #define FRAME_INTERVAL  25
 #define EFFECT_INTERVAL 10000
 
+#define MAGIC           0xBEAC07
+
 class Controller {
     public:
         Controller();
@@ -25,6 +27,16 @@ class Controller {
 
         void update();
 
+        typedef struct beacon {
+            uint32_t magic;
+            uint32_t millis;
+            uint16_t effect;
+            uint32_t param1;
+            uint32_t param2;
+            uint32_t param3;
+            uint32_t param4;
+        };
+
     private:
         PixelMatrix *pm;
 
@@ -34,6 +46,8 @@ class Controller {
         StrobeEffect *strobeEffect;
 
         Effect *currentEffect = NULL;
+
+        uint32_t effectParams = [0, 0, 0, 0];
 
         bool isMaster = true;
 
@@ -45,4 +59,9 @@ class Controller {
         uint32_t getMillis();
         void setEffect(Effect *effect, uint32_t param1 = 0, uint32_t param2 = 0, uint32_t param3 = 0, uint32_t param4 = 0);
         void nextEffect();
+
+        Effect* getEffectById(uint8_t id);
+
+        void broadcastStatus();
+        void handleBeacon(const uint8_t * mac, const uint8_t *data, int len);
 };
