b0a0a283b9260d1f374dbc31d5e4c3a603f139d2
János Arenhövel <jaarenhoevel@gmail.com>
2023-10-09 18:33:44 +0200

    Make effect a little bit more interesting

--- lib/Effects/RainbowEffect.cpp
+++ lib/Effects/RainbowEffect.cpp
@@ -9,8 +9,9 @@ class RainbowEffect: public Effect {
     public: void init(bool isMaster, uint32_t param1 = 0, uint32_t param2 = 0, uint32_t param3 = 0, uint32_t param4 = 0) {        
         Effect::init(isMaster, param1, param2, param3, param4);
         
-        fill_solid(pm->filter, PIXEL_COUNT, CRGB::White);
-        fill_rainbow_circular(pm->pixels, PIXEL_COUNT, 0);
+        fill_rainbow_circular(this->pm->filter, PIXEL_COUNT, 0);
+        fill_gradient_RGB(this->pm->pixels, PIXEL_COUNT / 2, CRGB::Black, CRGB::Black, CRGB::White, CRGB::Black);
+        fill_gradient_RGB(&this->pm->pixels[PIXEL_COUNT / 2], PIXEL_COUNT / 2, CRGB::Black, CRGB::Black, CRGB::White, CRGB::Black);
 
         pm->setRotation(param1, param1 != 0);    
     };
